6c3d2c2ee7c16e5646b3400f8ada1105828b49e0
Angry-Hamster <krutsvanya@gmail.com>
2021-01-08 19:02:25 +0200

    fix year

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { log } from "handlebars";
 import css from "./css/style.css";
 import timerRafs from './refs/timerRafs.js'
-
+console.log('Осталось до Нового Года');
 function getTimeBetween(){
-  let timeBetween = +new Date(`Jan 1 2021`) - new Date()
+  let nowYear = new Date().getFullYear()
+
+  let timeBetween = +new Date(`Jan 1 ${++nowYear}`) - new Date()
 
   let daysBrfore = Math.floor(timeBetween / 1000 / 60 / 60 /24)
   let hoursBefore = Math.floor(timeBetween % (1000 * 60 * 60 * 24) / (1000 * 60 * 60 ))
@@ -17,4 +19,3 @@ function getTimeBetween(){
 }
 
 let interval = setInterval(getTimeBetween, 1000)
-
